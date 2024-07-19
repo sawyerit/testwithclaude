b6d47d4331cf68eb8e2aea5ac6d87ffade12aa96
Shane Sawyer <shane.sawyer@gmail.com>
2024-07-19 15:34:02 -0600

    Add random color generation feature

---
+++ src/Colors.ts
@@ -0,0 +1,12 @@
+class Colors {
+  static getRandomColor(): string {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+}
+
+export default Colors;
